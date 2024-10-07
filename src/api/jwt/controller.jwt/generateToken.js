69ef343402aa2701baa76b266f07d89db7ece151
taesik <xmflr28@gmail.com>
2024-10-07 20:22:36 +0900

    jwt/generate 호출 시 user_id 경로 수정

--- src/api/jwt/controller.jwt/generateToken.js
+++ src/api/jwt/controller.jwt/generateToken.js
@@ -6,7 +6,7 @@ const findUserById = require('../../../database/user/dao/findUserById');
 
 module.exports = async (req, res, next) => {
   try {
-    const user_id = req.user_id
+    const user_id = req.user_id || req.body.user_id
 
     const is_user = await findUserById(user_id)
     if (!is_user) {
