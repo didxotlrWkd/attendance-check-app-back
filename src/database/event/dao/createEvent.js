696314b971f2ae38aab5ffd226f6f5291c775c6c
Ubuntu <ubuntu@ip-172-31-33-185.ap-northeast-2.compute.internal>
2024-10-15 18:34:56 +0900

    행사 생성 시 한국시간에 맞게 수정

--- src/database/event/dao/createEvent.js
+++ src/database/event/dao/createEvent.js
@@ -2,14 +2,18 @@ const { Event } = require('../..');
 
 module.exports = async ({ event_code,description, event_name, location, event_start_time,event_end_time }) => {
     try {
+
+        const event_start_time_utc = new Date(new Date(event_start_time).getTime() - (9 * 60 * 60 * 1000));
+        const event_end_time_utc = new Date(new Date(event_end_time).getTime() - (9 * 60 * 60 * 1000));
+
         // 새로운 이벤트 생성
         const event = await Event.create({
             event_code,
             description,  
             event_name,    
             location,      
-            event_start_time,
-            event_end_time     
+            event_start_time: event_start_time_utc,
+            event_end_time: event_end_time_utc
         });
 
         return event; 
