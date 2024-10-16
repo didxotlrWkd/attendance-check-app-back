630ec729161c5a776b8c6128129f92fb7bc18c37
Ubuntu <ubuntu@ip-172-31-33-185.ap-northeast-2.compute.internal>
2024-10-16 16:54:45 +0900

    학생 참여횟수 추가 방식 수정

--- src/database/participant/dao/findParticipantStartingWithTalkConcert.js
+++ src/database/participant/dao/findParticipantStartingWithTalkConcert.js
@@ -3,7 +3,7 @@ const { Op } = require('sequelize');
 
 module.exports = async(event_code, user_id) => {
     try {
-        const results = await Participant.findAll({
+        const results = await Participant.findOne({
             where: {
                 event_code: {
                     [Op.like]: 'SCHUSWCU1stAF_GraduatedTalkConcert%' 
