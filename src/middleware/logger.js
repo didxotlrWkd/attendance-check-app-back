12675f608c755bd76accd340e52baedc6f986e71
taesik <xmflr28@gmail.com>
2024-11-02 01:23:30 +0900

    로그 기록 추가

---
+++ src/middleware/logger.js
@@ -0,0 +1,16 @@
+// logger.js
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.File({ filename: 'combined.log' }),
+        new winston.transports.Console() // 콘솔에도 로그 출력
+    ],
+});
+
+module.exports = logger;
