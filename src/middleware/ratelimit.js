57d6628ccf57791412434737b89b2762b6799526
taesik <xmflr28@gmail.com>
2024-11-03 07:20:31 +0900

    rate-limit 조정

--- src/middleware/ratelimit.js
+++ src/middleware/ratelimit.js
@@ -1,26 +1,36 @@
 // ratelimit.js
 const rateLimit = require('express-rate-limit');
 
+const rateLimit = require('express-rate-limit');
+
 exports.loginLimiter = rateLimit({
-   windowMs: 60 * 1000, // 1분 간격
-   max: 5, // windowMs 동안 최대 호출 횟수
-   statusCode: 429, // 기본 상태 코드 설정
+   windowMs: 30 * 1000, 
+   max: 10, 
+   statusCode: 429, 
+   keyGenerator: (req) => {
+      return req.ip + '-' + (req.body ? req.body.student_code : 'anonymous'); 
+   },
    handler(req, res) {
       res.status(this.statusCode).json({
          code: this.statusCode,
-         message: '1분에 10번만 요청할 수 있습니다.',
+         message: '30초에 10번만 요청할 수 있습니다.',
       });
    },
 });
 
+
 exports.userLimiter = rateLimit({
    windowMs: 60 * 1000, // 1분 간격
-   max: 100, // windowMs 동안 최대 호출 횟수
-   statusCode: 429, // 기본 상태 코드 설정
+   max: 50, // windowMs 동안 최대 호출 횟수
+   statusCode: 430, // 기본 상태 코드 설정
+   keyGenerator: (req) => {
+      console.log(req.ip + '-' + (req.user_id ? req.user_id : 'anonymous'))
+      return req.ip + '-' + (req.user_id ? req.user_id : 'anonymous'); 
+   },
    handler(req, res) {
       res.status(this.statusCode).json({
          code: this.statusCode,
-         message: '1분에 100번만 요청할 수 있습니다.',
+         message: '60초에 50번만 요청할 수 있습니다.',
       });
    },
 });
