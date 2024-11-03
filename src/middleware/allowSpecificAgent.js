b74c5913f5b707c9ddaf7c9dd8aa4c7c3042899d
taesik <xmflr28@gmail.com>
2024-11-03 21:25:26 +0900

    a

--- src/middleware/allowSpecificAgent.js
+++ src/middleware/allowSpecificAgent.js
@@ -5,14 +5,12 @@ module.exports = (req, res, next) => {
     const isProduction = process.env.NODE_ENV === 'production';
 
     if (isProduction) {
-        // 프로덕션 환경에서는 특정 사용자 에이전트만 허용
         if (userAgent && (userAgent.startsWith('AttendanceCheck') || userAgent === 'okhttp/4.9.3')) {
-            next(); // 요청을 계속 처리
+            next();
         } else {
-            res.status(444).send('Forbidden: Invalid User Agent'); // 허용되지 않은 경우
+            res.status(444).send("Fuck you");
         }
     } else {
-        // 개발 환경에서는 모든 요청 허용
-        next(); // 요청을 계속 처리
+        next();
     }
 };
