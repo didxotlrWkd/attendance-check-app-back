47d126f35d2dc224c258bb76001c55bd7727b540
taesik <xmflr28@gmail.com>
2024-11-28 03:17:52 +0900

    .

--- src/middleware/allowSpecificAgent.js
+++ src/middleware/allowSpecificAgent.js
@@ -7,9 +7,7 @@ module.exports = (req, res, next) => {
     if (isProduction) {
         if (userAgent && (userAgent.startsWith('AttendanceCheck') || userAgent === 'okhttp/4.9.3')) {
             next();
-        } else if(req.ip === '121.152.103.101'){
-            return res.status(444).send("정준영씨 한번만 봐주세요")
-        }
+        } 
         else {
             return res.status(444).send("FUCK YOU");
         }
--- src/routes/index.js
+++ src/routes/index.js
@@ -15,52 +15,8 @@ router.get('/' , (req,res) => {
 })
 
 router.get('/favicon.ico', (req, res) => {
-    res.sendStatus(204); // 빈 응답
+    res.sendStatus(204); 
 });
-
-// //error test
-
-// router.get('/429', (req, res) => {
-//     res.status(429).send('Too Many Login Requests');
-// });
-
-// router.get('/430', (req,res) => {
-//     res.status(430).send('Too Many Requests')
-// })
-
-// router.get('/401', (req,res) => {
-//     res.status(401).send('유효하지 않은 토큰')
-// })
-
-
-// router.get('/420', (req, res) => {
-//     res.status(420).send('Enhance Your Calm');
-// });
-
-// router.get('/451', (req, res) => {
-//     res.status(451).send('존재하지않는코드');
-// });
-
-// router.get('/452', (req, res) => {
-//     res.status(452).send('이미 등록된 코드');
-// });
-
-
-// router.get('/405', (req, res) => {
-//     res.status(405).send('비밀번호 틀림');
-// });
-
-// router.get('/406', (req, res) => {
-//     res.status(406).send('회원정보 불일치');
-// });
-
-// router.get('/412', (req, res) => {
-//     res.status(412).send('중복로그인');
-// });
-
-// router.get('/409', (req, res) => {
-//     res.status(409).send('삭제된 계정');
-// });
 
 
 module.exports = router;
